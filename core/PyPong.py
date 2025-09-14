828a7d83b68c729e3fa1ce91b362dd21453e1a9f
Gwe2B <gwenael2b@live.fr>
2025-09-14 11:13:34 +0200

    feat: Adding pause menu and freeze game
    Adding pause menu on the keypress P
    
    Adding game freeze logic of 3s on every scoring + on the start of the game.

--- core/PyPong.py
+++ core/PyPong.py
@@ -1,6 +1,7 @@
 import arcade
-
 import random
+import time
+
 from core.Ball import Ball
 from core.Player import Player
 from core.constants import WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, PLAYER1_KEYS, PLAYER2_KEYS
@@ -21,6 +22,9 @@ class PyPong(arcade.Window):
         self.player_sprite_list.append(self.player1)
         self.player_sprite_list.append(self.player2)
 
+        self.is_game_paused = False
+        self.freeze_game()
+
         arcade.set_background_color(arcade.csscolor.BLACK)
 
     def on_ball_side_wall_hit(self, side: str):
@@ -33,6 +37,11 @@ class PyPong(arcade.Window):
         # Réinitialise la balle au centre
         self.ball.velocity_y = random.uniform(-0.5, 0.5)
         self.ball.reset()
+        self.freeze_game()
+
+    def freeze_game(self):
+        self.freeze_start_time = time.time()
+        self.is_game_freezed = True
 
     def on_draw(self):
         self.clear()
@@ -64,10 +73,50 @@ class PyPong(arcade.Window):
             anchor_x="center"  # Centre le texte horizontalement
         )
 
+        if self.is_game_freezed:
+            self.draw_freeze()
+
+        if self.is_game_paused:
+            self.draw_pause_menu()
+
+    def draw_freeze(self):
+        count = int(time.time() - self.freeze_start_time)
+        # Draw semi-transparent black overlay
+        arcade.draw_lrbt_rectangle_filled(
+            0,  WINDOW_WIDTH, 0, WINDOW_HEIGHT,
+            (0, 0, 0, 150)  # RGBA: Black with alpha (transparency)
+        )
+
+        arcade.draw_text(
+            f"{3 - count}", WINDOW_WIDTH/2, WINDOW_HEIGHT/2,
+            arcade.color.WHITE, 128, anchor_x="center", anchor_y="center"
+        )
+
+        if count >= 3:
+            self.is_game_freezed = False
+    
+    def draw_pause_menu(self):
+        # Draw semi-transparent black overlay
+        arcade.draw_lrbt_rectangle_filled(
+            0,  WINDOW_WIDTH, 0, WINDOW_HEIGHT,
+            (0, 0, 0, 150)  # RGBA: Black with alpha (transparency)
+        )
+        arcade.draw_text(
+            "PAUSE\nPress P to unpause", WINDOW_WIDTH/2, WINDOW_HEIGHT/2,
+            arcade.color.WHITE, 64, width=600, align="center",
+            anchor_x="center", anchor_y="center", multiline=True
+        )
+
     def on_update(self, delta_time):
-        self.ball_sprite_list.update()
+        if self.is_game_paused:
+            return
+
         self.player_sprite_list.update()
 
+        if self.is_game_freezed:
+            return
+
+        self.ball_sprite_list.update()
         ball_hit_list = arcade.check_for_collision_with_list(self.ball, self.player_sprite_list)
         if len(ball_hit_list) > 0:
             self.ball.bounce()
@@ -75,6 +124,9 @@ class PyPong(arcade.Window):
     def on_key_press(self, symbol, modifiers):
         self.player1.register_key_press(symbol)
         self.player2.register_key_press(symbol)
+
+        if symbol == arcade.key.P:
+            self.is_game_paused = not self.is_game_paused
 
     def on_key_release(self, symbol, modifiers):
         self.player1.register_key_release(symbol)
